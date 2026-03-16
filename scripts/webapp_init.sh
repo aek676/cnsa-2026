3b72ac869c35b8f2a2333f72073a0a5481a9ec2c
aek676 <aek676@inlumine.ual.es>
2026-03-16 11:46:18 +0100

    feat(webapp): fix Docker repo source list formatting
    Remove unnecessary backslash and space in Docker repository source list
    command to improve readability and ensure proper shell execution. This
    change does not affect functionality but aligns with shell best
    practices.

--- scripts/webapp_init.sh
+++ scripts/webapp_init.sh
@@ -17,8 +17,8 @@ chmod a+r /etc/apt/keyrings/docker.asc
 # Add the Docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Install Docker and related packages
 apt-get update
